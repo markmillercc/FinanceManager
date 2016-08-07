3fcb9033424d586438a95dafd2f549aca6876282
markmillerio <markmiller885@gmail.com>
2016-08-07 10:28:45 -0700

    inital commit

---
+++ FinanceManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinanceManager.Startup))]
+namespace FinanceManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
